fe4bc2fce3c422b995f16e39ed465c863f24d984
sebachaves <seba.-chaves@hotmail.com>
2017-10-28 16:52:12 -0300

    ASD

--- RegistroPago/CargaFactura.Designer.cs
+++ RegistroPago/CargaFactura.Designer.cs
@@ -28,19 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.numeroFacturaLabel = new System.Windows.Forms.Label();
+            this.fechaCobroLabel = new System.Windows.Forms.Label();
+            this.empresaLabel = new System.Windows.Forms.Label();
+            this.clienteLabel = new System.Windows.Forms.Label();
+            this.fechaVencimientoLabel = new System.Windows.Forms.Label();
+            this.importeLabel = new System.Windows.Forms.Label();
+            this.sucursalLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // numeroFacturaLabel
+            // 
+            this.numeroFacturaLabel.AutoSize = true;
+            this.numeroFacturaLabel.Location = new System.Drawing.Point(76, 33);
+            this.numeroFacturaLabel.Name = "numeroFacturaLabel";
+            this.numeroFacturaLabel.Size = new System.Drawing.Size(83, 13);
+            this.numeroFacturaLabel.TabIndex = 0;
+            this.numeroFacturaLabel.Text = "Numero factura:";
+            // 
+            // fechaCobroLabel
+            // 
+            this.fechaCobroLabel.AutoSize = true;
+            this.fechaCobroLabel.Location = new System.Drawing.Point(76, 70);
+            this.fechaCobroLabel.Name = "fechaCobroLabel";
+            this.fechaCobroLabel.Size = new System.Drawing.Size(85, 13);
+            this.fechaCobroLabel.TabIndex = 1;
+            this.fechaCobroLabel.Text = "Fecha de cobro:";
+            // 
+            // empresaLabel
+            // 
+            this.empresaLabel.AutoSize = true;
+            this.empresaLabel.Location = new System.Drawing.Point(76, 107);
+            this.empresaLabel.Name = "empresaLabel";
+            this.empresaLabel.Size = new System.Drawing.Size(51, 13);
+            this.empresaLabel.TabIndex = 2;
+            this.empresaLabel.Text = "Empresa:";
+            // 
+            // clienteLabel
+            // 
+            this.clienteLabel.AutoSize = true;
+            this.clienteLabel.Location = new System.Drawing.Point(76, 144);
+            this.clienteLabel.Name = "clienteLabel";
+            this.clienteLabel.Size = new System.Drawing.Size(42, 13);
+            this.clienteLabel.TabIndex = 3;
+            this.clienteLabel.Text = "Cliente:";
+            // 
+            // fechaVencimientoLabel
+            // 
+            this.fechaVencimientoLabel.AutoSize = true;
+            this.fechaVencimientoLabel.Location = new System.Drawing.Point(76, 181);
+            this.fechaVencimientoLabel.Name = "fechaVencimientoLabel";
+            this.fechaVencimientoLabel.Size = new System.Drawing.Size(115, 13);
+            this.fechaVencimientoLabel.TabIndex = 4;
+            this.fechaVencimientoLabel.Text = "Fecha de vencimiento:";
+            // 
+            // importeLabel
+            // 
+            this.importeLabel.AutoSize = true;
+            this.importeLabel.Location = new System.Drawing.Point(76, 218);
+            this.importeLabel.Name = "importeLabel";
+            this.importeLabel.Size = new System.Drawing.Size(45, 13);
+            this.importeLabel.TabIndex = 5;
+            this.importeLabel.Text = "Importe:";
+            // 
+            // sucursalLabel
+            // 
+            this.sucursalLabel.AutoSize = true;
+            this.sucursalLabel.Location = new System.Drawing.Point(76, 255);
+            this.sucursalLabel.Name = "sucursalLabel";
+            this.sucursalLabel.Size = new System.Drawing.Size(51, 13);
+            this.sucursalLabel.TabIndex = 6;
+            this.sucursalLabel.Text = "Sucursal:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(197, 63);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(197, 175);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(197, 215);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
             // 
             // CargaFactura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(445, 332);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sucursalLabel);
+            this.Controls.Add(this.importeLabel);
+            this.Controls.Add(this.fechaVencimientoLabel);
+            this.Controls.Add(this.clienteLabel);
+            this.Controls.Add(this.empresaLabel);
+            this.Controls.Add(this.fechaCobroLabel);
+            this.Controls.Add(this.numeroFacturaLabel);
             this.Name = "CargaFactura";
             this.Text = "Carga Factura";
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         #endregion
+        private System.Windows.Forms.Label numeroFacturaLabel;
+        private System.Windows.Forms.Label fechaCobroLabel;
+        private System.Windows.Forms.Label empresaLabel;
+        private System.Windows.Forms.Label clienteLabel;
+        private System.Windows.Forms.Label fechaVencimientoLabel;
+        private System.Windows.Forms.Label importeLabel;
+        private System.Windows.Forms.Label sucursalLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
